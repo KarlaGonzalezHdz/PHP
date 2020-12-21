9b4fc1909d268c1d35c1498d5428966b0f3eceff
Karla GH <karlahdz.98@outlook.com>
2020-12-21 14:35:15 -0600

    echo , gettype() , variables

---
+++ PHP-MySQL/hola_mundo.php
@@ -0,0 +1,22 @@
+<?php
+    //Comentario de una linea
+    # otro comentario
+    /* Otro comentario de mas 
+    de dos lineas */
+
+    //Echo nos sirve para mostrar información.
+   // echo "Hola mundo";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <!-- <h1>Hola soy un h1 </h1> -->
+   <h1><?php  echo "Hola mundo"; ?></h1>
+</body>
+</html>
---
+++ PHP-MySQL/variables.php
@@ -0,0 +1,25 @@
+<?php
+
+    #String : Cadena de texto
+    $nombre = "Karla";
+    #Integer: Numeros enteros
+    $numero = 3;
+
+    #Double: Numeros con decimales
+    $numero_decimal = 7.7;
+
+    #Boolean: Verdadero o Falso (True / False)
+    $verdadero = true;
+
+    #Array: Arreglo
+    #Object : Objeto
+    #Class: Clase
+    #Null : Cuando a una variable aun no se le ha asignado ningun valor
+
+
+    echo "Hola, $nombre <br>"; //puedes mandar a llamar variables.
+    echo 'Hola,  '.$nombre.'<br>'; // forma comun de usar 
+
+     echo gettype($nombre); //FUNCION PARA SABER QUE TIPO DE VARIABLES QUE ES
+    
+?>
