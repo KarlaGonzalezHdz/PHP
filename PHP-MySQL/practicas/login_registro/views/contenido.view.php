a1b644473ee882df766805ea6259dd02696fc547
Karla GH <karlahdz.98@outlook.com>
2021-01-07 15:22:18 -0600

    inicio de la practica de login - esqueleto y estilos

---
+++ PHP-MySQL/practicas/login_registro/views/contenido.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contenido</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Raleway:wght@100&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+    <div class="contenedor">
+        <h1 class="titulo"> Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis commodi accusantium
+                    consequatur, totam laboriosam impedit alias! Adipisci placeat minima, incidunt aliquam dolorum dolor
+                    nemo sunt illo accusantium cumque pariatur. Lorem ipsum dolor sit amet consectetur, adipisicing
+                    elit. Quae, impedit eum. Doloribus, voluptatum! Id minus cum nesciunt maiores, ea facilis
+                    recusandae. Minima ducimus eos tempora tenetur? Omnis enim saepe architecto!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis commodi accusantium
+                    consequatur, totam laboriosam impedit alias! Adipisci placeat minima, incidunt aliquam dolorum dolor
+                    nemo sunt illo accusantium cumque pariatur. Lorem ipsum dolor sit amet consectetur, adipisicing
+                    elit. Quae, impedit eum. Doloribus, voluptatum! Id minus cum nesciunt maiores, ea facilis
+                    recusandae. Minima ducimus eos tempora tenetur? Omnis enim saepe architecto!</p>
+            </article>
+        </div>
+
+
+    </div>
+</body>
+
+</html>
