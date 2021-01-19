849fb4d2a081e4f45d7a66553144bfc3c47bd9c6
Karla GH <karlahdz.98@outlook.com>
2021-01-19 14:18:08 -0600

    javascript

---
+++ PHP-MySQL/AJAX/js/json.js
@@ -0,0 +1,31 @@
+
+    var karla = {
+        "nombre": 'Karla Gonzalez',
+        "edad": 23 ,
+        "pais": "Mexico"
+    }
+
+    var pedro = {
+        "nombre": 'Pedro Obregon',
+        "edad": 31 ,
+        "pais": "Colombia"
+    }
+
+    //ARREGLOS
+    var nombreAmigos = ['Alejando', 'Manuel']
+
+    [
+        {
+            "nombre": "Alejandro",
+            "edad": 24,
+            "pais": "España"
+        }, 
+        {
+            "nombre": "Manuel",
+            "edad": 30,
+            "pais": "Colombia"
+        }
+    ]
+    
+
+    console.log(nombreAmigos[1]);
