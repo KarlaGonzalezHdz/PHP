48f0cafaccfe359bd967c01b677d604bd5dd6b2b
Karla GH <karlahdz.98@outlook.com>
2021-02-03 13:58:25 -0600

    var_dump()

--- MasterPHP/IntroduccionPHP/02.Variables.php
+++ MasterPHP/IntroduccionPHP/02.Variables.php
@@ -5,30 +5,42 @@
     
     echo "Esto es una variable numero: $numero" ;
     echo "<br>";
+    var_dump($numero);
+    echo "<br>";
 
     #Variable texto
     $palabra = "palabra";
     echo "Esto es una variable de texto con valor de $palabra";
+    echo "<br>";
+    var_dump($palabra);
     echo "<br>";
 
     #variable boleana
     $boleana = true; #verdadero -> 1 / falso -> 0
     echo "Esto es una variable boleana: $boleana";
     echo "<br>";
+    var_dump($boleana);
+    echo "<br>";
     
     #variable Arreglo
     $arreglo = array("rojo", "amarillo");
     echo "Esto es una variable arreglo: $arreglo[0]";
+    echo "<br>";
+    var_dump($arreglo);
     echo "<br>";
 
     #variable Arreglo con propiedades
     $verduras = array("verdura1"=>"lechuga", "verdura2"=>"cebolla");
     echo "Esto es una variable arreglo con propiedades: $verduras[verdura2]";
     echo "<br>";
+    var_dump($verduras);
+    echo "<br>";
     
     #variable objeto
     $frutas = (object)["fruta1"=>"pera", "fruta2"=>"manzana"];
     echo "Esto es una variable objeto: $frutas->fruta1";
+    echo "<br>";
+    var_dump($frutas);
     echo "<br>";
 
 ?>
